94c8ffe3f034359fa8d62ecff047803328e1ce32
Ankur Sethi <contact@ankursethi.in>
2013-07-17 13:12:37 +0530

    Grab a random message from SimpleStorage every time the page is loaded.

--- data/content_script.js
+++ data/content_script.js
@@ -1,16 +1,22 @@
 (function() {
-  var msgDiv = document.createElement('div');
-  msgDiv.id = 'youshouldprobably-msgdiv';
-  msgDiv.className += 'youshouldprobably-msgdiv';
-  msgDiv.innerHTML = '<div class="youshouldprobably-msg">You should probably read a book.' +
-                     '  <span class="youshouldprobably-close" id="youshouldprobably-close">close</span>' +
-                     '</div>';
 
-  var body = document.getElementsByTagName('body')[0];
-  body.insertBefore(msgDiv, body.childNodes[0]);
+  function addMessageBox(message) {
+    var msgDiv = document.createElement('div');
+    msgDiv.id = 'youshouldprobably-msgdiv';
+    msgDiv.className += 'youshouldprobably-msgdiv';
+    msgDiv.innerHTML = '<div class="youshouldprobably-msg">' + message +
+                       '  <span class="youshouldprobably-close" id="youshouldprobably-close">close</span>' +
+                       '</div>';
+
+    var body = document.getElementsByTagName('body')[0];
+    body.insertBefore(msgDiv, body.childNodes[0]);
   
-  var closeBtn = document.getElementById('youshouldprobably-close');
-  closeBtn.addEventListener('click', function() {
-    msgDiv.style.opacity = 0;
-  });
+    var closeBtn = document.getElementById('youshouldprobably-close');
+    closeBtn.addEventListener('click', function() {
+      msgDiv.style.opacity = 0;
+    });
+  }
+
+  self.port.on('haveMessage', addMessageBox);
+  self.port.emit('wantMessage');
 })();
